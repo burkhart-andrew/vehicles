709e9a7ba0fe48c0e23bdb6847bb4bb0672f3914
Andrew <burkhart.andrew.j@gmail.com>
2015-11-08 18:47:13 -0500

    Adding first exercise

---
+++ car.rb
@@ -0,0 +1,78 @@
+class Car
+
+	MILES_PER_OIL_CHANGE = 4000
+
+	def initialize(make, model, year, miles, last_oil_change)
+		@make = make
+		@model = model
+		@year = year
+		@miles = miles
+		@last_oil_change = last_oil_change
+
+	end
+
+	def name()
+		"Your car is a #{@year} #{@make} #{@model} and has #{@miles} miles"
+	end
+
+	def drive(new_miles)
+		@miles = @miles + new_miles
+	end
+
+	def change_oil!
+		# change the oil
+	end
+
+	def oil_change_status()
+		#put message #1 else puts message #2
+		#condition = current mileage
+		miles_till = MILES_PER_OIL_CHANGE - (@miles - @last_oil_change) 
+		miles_since = @miles - @last_oil_change
+
+		if oil_needs_changing?
+			"It has been #{miles_since} miles since my last oil change. It needs to be changed!"
+		else 
+			"It has been #{miles_since} miles since my last oil change. I have another #{miles_till} miles before the next oil change!"
+		end
+
+	end
+
+	def oil_needs_changing?
+		@miles > @last_oil_change + MILES_PER_OIL_CHANGE
+		
+	end
+
+
+		
+end
+
+truck = Hash.new
+truck[:make] = 'Ford'
+truck[:model] = 'Ranger'
+truck[:year] = 1996
+
+sedan = {
+	make: 'Hyundai',
+	model: "Elantra",
+	year: "2011"
+}
+
+def vehicle_name(hsh)
+	"Your car is a #{hsh[:year]} #{hsh[:make]} #{hsh[:model]}"
+end
+
+van = Car.new("Dodge", "Grand Caravan", 2013, 68000, 66000)
+civic = Car.new("Honda", "Civic", 2013, 40200, 40000)
+
+puts "All our cars:"
+puts van.name
+puts civic.name
+
+van.drive(100)
+civic.drive(3799)
+
+puts van.name
+puts civic.name
+
+puts van.oil_change_status
+puts civic.oil_change_status
